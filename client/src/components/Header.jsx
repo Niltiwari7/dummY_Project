c0f860f5baeb8548fc02beeb734b3d1af3ae9d97
Niltiwari7 <nt465638@gmail.com>
2024-07-30 10:41:37 +0530

    Header design improve

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
     );
     gsap.fromTo(
       ".title",
-      { opacity: 0, x: -50 },
+      { opacity: 0, x: -150 },
       { opacity: 1, x: 0, duration: 1, delay: 0.5 }
     );
     gsap.fromTo(
@@ -23,38 +23,39 @@ export default function Header() {
 
   return (
     <header className="header bg-sky-500 shadow-md">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-2 sm:p-3">
         <Link to='/'>
           <h1 className="title font-bold text-sm sm:text-xl flex flex-wrap">
             <span className="text-white sm:text-4xl">MahaKal</span>
-            <span className="text-black-500 sm:text-xl">Construction</span>
+            <span className="text-gray-500 sm:text-xl">Construction</span>
           </h1>
         </Link>
-        <ul className="flex gap-4">
-          <li className="nav-item hidden sm:inline">
-            <button className="hover:bg-sky-700 text-white px-3 py-2 rounded sm:text-xl">
-              Home
-            </button>
-          </li>
-          <li className="nav-item hidden sm:inline">
-            <button className="hover:bg-sky-700 text-white px-3 py-2 sm:text-xl rounded">
-              About
-            </button>
-          </li>
-          <li className="nav-item sm:inline">
-            <button className="hover:bg-sky-700 text-white px-3 py-2 sm:text-xl rounded">
-              Sign In
-            </button>
-          </li>
-        </ul>
-        <form>
-          <input
-            type="text"
-            placeholder="Search.."
-            className="p-2 rounded border-2 border-white focus:outline-none focus:ring-2 focus:ring-sky-300"
-          />
-          
-        </form>
+        <div className="flex flex-1 justify-end items-center sm:ml-4 sm:mx-6">
+          <form className="hidden sm:block sm:mr-4">
+            <input
+              type="text"
+              placeholder="Search.."
+              className="p-2 rounded border-2 border-white focus:outline-none focus:ring-2 focus:ring-sky-300"
+            />
+          </form>
+          <ul className="hidden sm:flex gap-4">
+            <Link to='/' className="nav-item">
+              <button className="hover:bg-sky-700 text-white px-3 py-2 rounded sm:text-xl ">
+                Home
+              </button>
+            </Link>
+            <Link to='/about' className="nav-item">
+              <button className="hover:bg-sky-700 text-white px-3 py-2 sm:text-xl rounded">
+                About
+              </button>
+            </Link>
+          </ul>
+          <Link to='/sign-in'>
+          <button className="nav-item hover:bg-sky-700 text-white px-3 py-2 sm:text-xl rounded ml-4  ">
+            Sign In
+          </button>
+          </Link>
+        </div>
       </div>
     </header>
   );
